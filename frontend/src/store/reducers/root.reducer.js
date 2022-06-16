3b8adafa00ef8cd9ea0f60cf1a7d47a9cd6350c6
Ali Mahmudov <71690649+28tad@users.noreply.github.com>
2022-06-16 20:16:41 +0300

    Revert "Clown"

--- frontend/src/store/reducers/root.reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-// import { partyReducer } from './party.reducer';
-
-const rootReducer = combineReducers({
-  // part: partyReducer,
-});
-
-export default rootReducer;
