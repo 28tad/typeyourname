b9e9120fe96e186f0270f758d95f92cdc0657e17
John <nextadm@mail.ru>
2022-06-16 20:02:44 +0300

    Adaptiv homepage

--- frontend/src/components/register/Home.jsx
+++ frontend/src/components/register/Home.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import '../register/Home.css';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+// import { makeStyles } from '@mui/styles';
+// import { ThemeProvider, createTheme } from '@mui/material/styles';
+
 
 
 function Home() {
+
+
   return (
+
+
     <div className='row'>
       <img className='tree' src='tree.jpg' alt='tree'/>
-      <p className='type'>TYPE YOUR NAME</p>
+      <Box>
+      <Typography variant="h2" style={{
+      color: 'white', 
+      fontSize: '100px',
+      fontFamily: 'Source Sans Pro, sans-serif',
+      textAlign: 'center',
+      letterSpacing: '30px',
+      fontWeight: '1000',
+      
+
+      }} gutterBottom component="div">
+
+      TYPE YOUR NAME
+
+      </Typography>
+      </Box>
+      {/* <p className='type'>TYPE YOUR NAME</p> */}
       <input className='input' type="text" />
     </div>
   );
